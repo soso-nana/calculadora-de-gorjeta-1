c20337175cbcca50b4eb2b622fbb965ace88310e
soso-nana <sophia0315163n.v@gmail.com>
2026-05-23 18:54:58 -0300

    nono commit do CSS e terceiro commit do JS

--- script.js
+++ script.js
@@ -28,10 +28,16 @@ function receberQuantidadePessoas(evento){
 
 const botoesGorjeta = document.querySelectorAll(".gorjeta input[type='button']")
 botoesGorjeta.forEach(botao => {
-    botao.addEventListener("click", receberPorcetagem)
+    botao.addEventListener("click", receberPorcetagemBotao)
 })
 
-function receberPorcetagem(evento){
-    
-    porcentagem = Number(evento.target.value) / 100
+function receberPorcetagemBotao(evento){
+    botoesGorjeta.forEach(botao =>{
+        botao.classList.remove("botao-ativo")
+        if(botao.value === evento.target.value){
+            botao.classList.add("botao-ativo")
+        }
+    })
+
+    porcentagem = parseFloat(evento.target.value) / 100
 }
